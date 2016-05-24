05057593d9207b6bf31361b629038f427e20a39e
Jie Wen <wenjie1070116@gmail.com>
2016-05-24 18:55:41 -0400

    Intuitive 那个方法会超时；思考的时间略长

--- 220-Contains-Duplicate-III/solution.py
+++ 220-Contains-Duplicate-III/solution.py
@@ -9,9 +9,11 @@ class Solution(object):
         if not nums or len(nums) < 2 or k < 1 or t < 0: return False
         hashmap = {}
         for i in xrange(len(nums)):
-            cur = nums[i]/t if t > 0 else nums[i]
+            cur = nums[i]/t if t > 0 else nums[i] # take care of the situation if t == 0
             for j in (cur-1, cur, cur+1):
-                if j in hashmap and i-hashmap[j] <= k and abs(nums[hashmap[j]]-nums[i]) <= t:
+                # again check abs(nums[hashmap[j]]-nums[i]) <= t is very important cause when abs(n1-n2) is bigger than t but
+                # abs(n1/k-n2/k) can <= 1
+                if j in hashmap and i-hashmap[j] <= k and abs(nums[hashmap[j]]-nums[i]) <= t: 
                     return True
             hashmap[cur] = i
         return False
