9f66deed6d856b9d71584abae121d167362b87a9
Jie Wen <wenjie1070116@gmail.com>
2016-05-24 18:52:43 -0400

    Intuitive 那个方法会超时；思考的时间略长

--- 220-Contains-Duplicate-III/solution.py
+++ 220-Contains-Duplicate-III/solution.py
@@ -6,12 +6,12 @@ class Solution(object):
         :type t: int
         :rtype: bool
         """
-        if not nums or len(nums) < 2 or k < 1: return False
+        if not nums or len(nums) < 2 or k < 1 or t < 0: return False
         hashmap = {}
         for i in xrange(len(nums)):
-            cur = nums/k
+            cur = nums[i]/t if t > 0 else nums[i]
             for j in (cur-1, cur, cur+1):
-                if j in hashmap and i-hashmap[j] <= k and abs(hashmap[j]-nums[i]) <= t:
+                if j in hashmap and i-hashmap[j] <= k and abs(nums[hashmap[j]]-nums[i]) <= t:
                     return True
             hashmap[cur] = i
         return False
