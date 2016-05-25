9b8d4a9dbce4685f4c4e2421e184a0450f5447df
Jie Wen <wenjie1070116@gmail.com>
2016-05-25 01:39:23 -0400

    [33. Search in Rotated Sorted Array][Accepted]committed by LeetCode Extension

--- 33-Search-in-Rotated-Sorted-Array/solution.py
+++ 33-Search-in-Rotated-Sorted-Array/solution.py
@@ -6,7 +6,7 @@ class Solution(object):
         :rtype: int
         """
         if not nums: return -1
-        start, end = -1, len(nums)
+        start, end = 0, len(nums)-1
         while start + 1 < end:
             mid = start + (end-start)/2
             if nums[mid] >=target>=nums[start] or (nums[mid] < nums[start] and (target >= nums[start] or target <= nums[mid])):
