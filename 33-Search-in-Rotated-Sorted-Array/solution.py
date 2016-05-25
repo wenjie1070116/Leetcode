8421b44af69eca97bd914dda3f15e9bb5adc04f8
Jie Wen <wenjie1070116@gmail.com>
2016-05-25 01:38:57 -0400

    [33. Search in Rotated Sorted Array][Runtime Error]committed by LeetCode Extension

---
+++ 33-Search-in-Rotated-Sorted-Array/solution.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        if not nums: return -1
+        start, end = -1, len(nums)
+        while start + 1 < end:
+            mid = start + (end-start)/2
+            if nums[mid] >=target>=nums[start] or (nums[mid] < nums[start] and (target >= nums[start] or target <= nums[mid])):
+                end = mid
+            else:
+                start = mid
+        if nums[start] == target: return start
+        if nums[end] == target: return end
+        return -1
