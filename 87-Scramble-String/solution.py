93a77bff3d6b73ccea526dcdd619dad8bfc2f7a0
Jie Wen <wenjie1070116@gmail.com>
2016-05-26 00:56:14 -0400

    [87. Scramble String][Time Limit Exceeded]committed by LeetCode Extension

--- 87-Scramble-String/solution.py
+++ 87-Scramble-String/solution.py
@@ -8,6 +8,6 @@ class Solution(object):
         if s1 == s2: return True
         if len(s1) != len(s2): return False
         for i in xrange(1, len(s1)):
-            if (self.isScrambel(s1[:i], s2[:i]) and self.isScrambel(s1[i:], s2[i:])) or (self.isScramble(s1[:i], s2[len(s2)-i:len(s2)]) and self.isScrambel(s1[i:], s2[:len(s2)-i])):
+            if (self.isScramble(s1[:i], s2[:i]) and self.isScramble(s1[i:], s2[i:])) or (self.isScramble(s1[:i], s2[len(s2)-i:len(s2)]) and self.isScramble(s1[i:], s2[:len(s2)-i])):
                 return True
         return False
