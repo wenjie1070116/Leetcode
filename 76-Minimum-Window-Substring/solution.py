b923f26b4fd412ffd80a0e437dc50e9c00d2622c
Jie Wen <wenjie1070116@gmail.com>
2016-05-26 00:46:44 -0400

    [76. Minimum Window Substring][Accepted]committed by LeetCode Extension

--- 76-Minimum-Window-Substring/solution.py
+++ 76-Minimum-Window-Substring/solution.py
@@ -1,54 +1,26 @@
-import collections
-class Solution:
-    """
-    @param source: A string
-    @param target: A string
-    @return: A string denote the minimum window
-             Return "" if there is no such a string
-    """
-    def minWindow(self, source, target):
-        # write your code here
-        if not source or not target or len(source) < len(target): return ''
-        if source == target: return source
-        n = len(target)
-        count = collections.Counter(target)
-        total = 0
-        res = source
-        l, r, i = -1, -1, 0
-        while i < len(source):
-            if source[i] in count:
-                if l == -1:
-                    l = i
-                count[source[i]] -= 1
-                if count[source[i]] >= 0:
-                    total += 1
-                if total == n:
-                    r = i
-                    break
-            i += 1
-        if l == -1 or r == -1: return ''
-        if r-l+1 == 1: return source[l:r+1]
-        while l < r and (source[l] not in count or count[source[l]] < 0):
-            if source[l] in count:
-                count[source[l]] += 1
-            l += 1
-        if r-l+1 < len(res):
-            res = source[l:r+1]
-        while r < len(source):
-            count[source[l]] += 1
-            if count[source[l]] > 0:
-                total -= 1
-            l += 1
-            while l < r and (source[l] not in count or count[source[l]] < 0):
-                if source[l] in count:
-                    count[source[l]] += 1
-                l += 1
-            while r < len(source) and total < n:
-                r += 1
-                if r < len(source) and source[r] in count:
-                    count[source[r]] -= 1
-                    if count[source[r]] >= 0:
-                        total += 1
-            if total == n and r-l+1 < len(res):
-                res = source[l:r+1]
+class Solution(object):
+    def minWindow(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: str
+        """
+        if not s or not t or len(s) < len(t): return ''
+        counter = collections.Counter(t)
+        hashmap = {}
+        count = 0
+        start = 0
+        res = s+'#'
+        for i in xrange(len(s)):
+            if s[i] in counter:
+                hashmap[s[i]] = hashmap.get(s[i], 0)+1
+                if hashmap[s[i]] <= counter[s[i]]:
+                    count +=1
+                while s[start] not in counter or hashmap[s[start]] > counter[s[start]]:
+                    if s[start] in hashmap:
+                        hashmap[s[start]] -= 1
+                    start += 1
+                if count == len(t) and i-start+1 < len(res):
+                    res = s[start:i+1]
+        if res == s+'#': return ''
         return res
