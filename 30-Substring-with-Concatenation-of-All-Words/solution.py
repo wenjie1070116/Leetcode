5872f45f6a2c242fda6b917693597366bfc39227
Jie Wen <wenjie1070116@gmail.com>
2016-05-26 00:26:32 -0400

    intuitive的方法超时

--- 30-Substring-with-Concatenation-of-All-Words/solution.py
+++ 30-Substring-with-Concatenation-of-All-Words/solution.py
@@ -7,6 +7,39 @@ class Solution(object):
         """
         if not s or not words: return []
         m, n = len(words), len(words[0])
+        Words = collection.Counter(words)
+        res = []
+        resset = set()
+        for i in xrange(len(s)-m*n+1):
+            if i in resset:
+                continue
+            hashmap = {}
+            count = 0
+            left = i
+            for j in xrange(i, len(s)-n+1, n):
+                cur = s[j:j+n]
+                if cur in Words:
+                    hashmap[cur] = hashmap.get(cur, 0)+1
+                    count += 1
+                    while hashmap[cur] > Words[cur]:
+                        hashmap[s[left:left+n]] -= 1
+                        count -= 1
+                        left += n
+                    if count == m:
+                        res.append(left)
+                        resset.add(left)
+                else:
+                    left = j+n
+                    count = 0
+                    hashmap = {}
+        return res
+                    
+            
+        
+        """
+        # TLE
+        if not s or not words: return []
+        m, n = len(words), len(words[0])
         
         def dfs(pos, wordhash):
             if not wordhash:
@@ -26,5 +59,6 @@ class Solution(object):
             if dfs(i, wordhash):
                 res.append(i)
         return res
+        """
                 
                 
