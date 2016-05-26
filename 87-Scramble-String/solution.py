a87cfe4c8a993f64b5084aa780c7ae6dd7fadfa5
Jie Wen <wenjie1070116@gmail.com>
2016-05-26 00:55:47 -0400

    [87. Scramble String][Runtime Error]committed by LeetCode Extension

---
+++ 87-Scramble-String/solution.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def isScramble(self, s1, s2):
+        """
+        :type s1: str
+        :type s2: str
+        :rtype: bool
+        """
+        if s1 == s2: return True
+        if len(s1) != len(s2): return False
+        for i in xrange(1, len(s1)):
+            if (self.isScrambel(s1[:i], s2[:i]) and self.isScrambel(s1[i:], s2[i:])) or (self.isScramble(s1[:i], s2[len(s2)-i:len(s2)]) and self.isScrambel(s1[i:], s2[:len(s2)-i])):
+                return True
+        return False
