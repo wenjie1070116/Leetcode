2f6b87d333ebfe003afe49bd373ee6c118bbbc47
Jie Wen <wenjie1070116@gmail.com>
2016-05-27 23:39:39 -0400

    [326. Power of Three][Accepted]committed by LeetCode Extension

--- 326-Power-of-Three/solution.py
+++ 326-Power-of-Three/solution.py
@@ -1,12 +1,13 @@
+import math
 class Solution(object):
     def isPowerOfThree(self, n):
         """
         :type n: int
         :rtype: bool
         """
-        if n < 0: return False
-        idx = int(math.log(n, 3))
-        if 3^idx == n:
+        if n <= 0: return False
+        idx = round(math.log(n, 3))
+        if 3**idx == n:
             return True
         return False
         
