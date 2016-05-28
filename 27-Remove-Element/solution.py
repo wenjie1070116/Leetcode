0ea6a7b24dbbcce504f27a9bb12d40d9a12ffe93
Jie Wen <wenjie1070116@gmail.com>
2016-05-28 01:41:13 -0400

    [27. Remove Element][Runtime Error]committed by LeetCode Extension

---
+++ 27-Remove-Element/solution.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        if not nums: return nums
+        count = 0
+        for i in xrange(len(nums)):
+            if nums[i] == val:
+                count += 1
+            else:
+                nums[i-count] = nums[i]
+        return len(nums)-count
