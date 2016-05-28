fe82a38b583952da591d291a4ade3a99d784549d
Jie Wen <wenjie1070116@gmail.com>
2016-05-28 01:38:12 -0400

    [107. Binary Tree Level Order Traversal II][Accepted]committed by LeetCode Extension

---
+++ 107-Binary-Tree-Level-Order-Traversal-II/solution.py
@@ -0,0 +1,30 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def levelOrderBottom(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+        if not root: return []
+        nodes = [root]
+        res = [[root.val]]
+        while nodes:
+            new = []
+            temp = []
+            for node in nodes:
+                if node.left:
+                    new.append(node.left)
+                    temp.append(node.left.val)
+                if node.right:
+                    new.append(node.right)
+                    temp.append(node.right.val)
+            if temp:
+                res.append(temp)
+            nodes = new
+        return res[::-1]
