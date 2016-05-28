bc126fe7146fa0279b0597e7dcf1dcf4f94164b4
Jie Wen <wenjie1070116@gmail.com>
2016-05-27 23:59:05 -0400

    [263. Ugly Number][Runtime Error]committed by LeetCode Extension

---
+++ 263-Ugly-Number/solution.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def isUgly(self, num):
+        """
+        :type num: int
+        :rtype: bool
+        """
+        if num <= 0:
+            return False
+        while num % 5 == 0:
+            num /= 5
+        while num % 3 == 0:
+            num /= 3
+        while num % 2 == 0:
+            num /= 2
+        return nums == 1
+        
