80cec70f781b3f77218c8723a25685b32f7f3bc5
Jie Wen <wenjie1070116@gmail.com>
2016-05-28 15:39:17 -0400

    [133. Clone Graph][Runtime Error]committed by LeetCode Extension

---
+++ 133-Clone-Graph/solution.py
@@ -0,0 +1,29 @@
+# Definition for a undirected graph node
+# class UndirectedGraphNode(object):
+#     def __init__(self, x):
+#         self.label = x
+#         self.neighbors = []
+
+class Solution(object):
+    def cloneGraph(self, node):
+        """
+        :type node: UndirectedGraphNode
+        :rtype: UndirectedGraphNode
+        """
+        copy = UndirectedGraphNode(node.label)
+        hashmap = {node.label:copy}
+        traversed = {}
+        stack = [node]
+        while stack:
+            cur = stack.pop()
+            for neighbor in cur.neighbors:
+                if neighbor.label in hashmap:
+                    cur.neighbors.append(hashmap[neighbor.label])
+                else:
+                    hashmap[cur.label] = UndirectedGraphNode(neighbor.label)
+                if neighbor.label not in traversed:
+                    stack.append(neighbor)
+            traversed.add(neighbor.label)
+        return copy
+                
+        
