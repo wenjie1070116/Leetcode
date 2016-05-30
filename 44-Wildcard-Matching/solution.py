7feef324baca2a710abb8a0c388782dc6ade89d9
Jie Wen <wenjie1070116@gmail.com>
2016-05-29 21:53:55 -0400

    [44. Wildcard Matching][Runtime Error]committed by LeetCode Extension

---
+++ 44-Wildcard-Matching/solution.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def isMatch(self, s, p):
+        """
+        :type s: str
+        :type p: str
+        :rtype: bool
+        """
+        if not s and not p:
+            return True
+        dp = [[False]*(len(p)+1) for _ in xrange(len(s)+1)]
+        dp[0][0] = True
+        for i in xrange(len(s)+1):
+            if p[0] == '*':
+                dp[i][1] = True
+        for i in xrange(len(s)):
+            for j in xrange(1, len(p)):
+                if s[i] == p[j] or p[j] == '?':
+                    dp[i+1][j+1] = dp[i][j]
+                elif p[j] == '*':
+                    for idx in xrange(i+1):
+                        if dp[idx+1][j]:
+                            dp[i+1][j+1] = True
+                            break
+        return dp[-1][-1]
