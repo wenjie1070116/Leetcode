9d4f2497ab5543970fa858571bda8441b6e69a23
Jie Wen <wenjie1070116@gmail.com>
2016-05-29 21:06:21 -0400

    [6. ZigZag Conversion][Runtime Error]committed by LeetCode Extension

---
+++ 6-ZigZag-Conversion/solution.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def convert(self, s, numRows):
+        """
+        :type s: str
+        :type numRows: int
+        :rtype: str
+        """
+        n = len(s) if s else 0
+        if n <= numRows or numRows == 1: return s
+        res = ''
+        for i in xrange(numRows):
+            idx = i
+            if idx == 0 or idx == numRows-1:
+                while idx < n:
+                    res += s[idx]
+                    idx += 2*numRows-2
+            else:
+                while idx < n:
+                    res += s[idx]
+                    idx += 2*numRows-2-2*i
+                    res += s[idx]
+                    idx += 2*i
+        return res
+        
