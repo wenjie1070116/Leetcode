a71bc8676c06e69dfe25a7dc7d4d328ef91892e0
Jie Wen <wenjie1070116@gmail.com>
2016-05-29 20:17:20 -0400

    [4. Median of Two Sorted Arrays][Runtime Error]committed by LeetCode Extension

--- 4-Median-of-Two-Sorted-Arrays/solution.py
+++ 4-Median-of-Two-Sorted-Arrays/solution.py
@@ -9,12 +9,12 @@ class Solution(object):
             return nums1[n-1]
         if n == 1:
             return min(nums1[0], nums2[0])
-        mid1 = nums1[(len1+1)/2-1]
-        mid2 = nums2[(len2+1)/2-1]
+        mid1 = nums1[len1/2-1]
+        mid2 = nums2[len2/2-1]
         if mid1 < mid2:
-            return self.findnth(nums1[(len1+1)/2:], nums2, n-(len1+1)/2)
+            return self.findnth(nums1[len1/2:], nums2, n-len1/2)
         else:
-            return self.findnth(nums1, nums2[(len2+1)/2:], n-(len2+1)/2)
+            return self.findnth(nums1, nums2[len2/2:], n-len2/2)
         
     def findMedianSortedArrays(self, nums1, nums2):
         """
@@ -26,6 +26,6 @@ class Solution(object):
         len1 = len(nums1) if nums1 else 0
         len2 = len(nums2) if nums2 else 0
         if (len1+len2)%2 == 1:
-            return self.findnth(nums1, nums2, (len1+len2)/2)
+            return self.findnth(nums1, nums2, (len1+len2+1)/2)
         else:
             return (self.findnth(nums1, nums2, (len1+len2)/2)+self.findnth(nums1, nums2, (len1+len2)/2+1))/2.0
