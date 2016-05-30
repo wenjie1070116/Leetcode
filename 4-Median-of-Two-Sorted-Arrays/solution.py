b5b0f09bdd7a4c380902eccd1369bbcb5a788ae0
Jie Wen <wenjie1070116@gmail.com>
2016-05-29 20:53:25 -0400

    [4. Median of Two Sorted Arrays][Accepted]committed by LeetCode Extension

--- 4-Median-of-Two-Sorted-Arrays/solution.py
+++ 4-Median-of-Two-Sorted-Arrays/solution.py
@@ -1,24 +1,19 @@
 class Solution(object):
-    def findnth(self, nums1, l1, r1, nums2, l2, r2, n):
-        if not nums1 and not nums2: return 
-        len1 = r1-l1+1
-        len2 = r2-l1+1
-        if l1 > r1:
-            return nums2[l2+n-1]
-        if l2 > r2:
-            return nums1[l1+n-1]
+    def findnth(self, nums1, nums2, n):
+        len1 = len(nums1) if nums1 else 0
+        len2 = len(nums2) if nums2 else 0
+        if len1 == 0:
+            return nums2[n-1]
+        if len2 == 0:
+            return nums1[n-1]
         if n == 1:
-            return min(nums1[l1], nums2[l2])
-        mid1 = nums1[(l1+r1)/2]
-        mid2 = nums2[(l2+r2)/2]
+            return min(nums1[0], nums2[0])
+        mid1 = nums1[n/2-1] if n/2-1 < len1 else sys.maxint
+        mid2 = nums2[n/2-1] if n/2-1 < len2 else sys.maxint
         if mid1 < mid2:
-            if n == (l1+r1)/2+1:
-                return mid1
-            return self.findnth(nums1, (l1+r1)/2+1, r1, nums2, l2, r2, n-(l1+r1)/2-1+l1)
+            return self.findnth(nums1[n/2:], nums2, n-n/2)
         else:
-            if n == (l2+r2)/2+1:
-                return mid2
-            return self.findnth(nums1, l1, r1, nums2, (l2+r2)/2+1, r2, n-(l2+r2)/2-1+l2)
+            return self.findnth(nums1, nums2[n/2:], n-n/2)
         
     def findMedianSortedArrays(self, nums1, nums2):
         """
@@ -30,6 +25,6 @@ class Solution(object):
         len1 = len(nums1) if nums1 else 0
         len2 = len(nums2) if nums2 else 0
         if (len1+len2)%2 == 1:
-            return self.findnth(nums1, 0, len1-1, nums2, 0, len2-1, (len1+len2+1)/2)
+            return self.findnth(nums1, nums2, (len1+len2+1)/2)
         else:
-            return (self.findnth(nums1, 0, len1-1, nums2, 0, len2-1, (len1+len2)/2)+self.findnth(nums1, 0, len1-1, nums2, 0, len2-1, (len1+len2)/2+1))/2.0
+            return (self.findnth(nums1, nums2, (len1+len2)/2)+self.findnth(nums1, nums2, (len1+len2)/2+1))/2.0
