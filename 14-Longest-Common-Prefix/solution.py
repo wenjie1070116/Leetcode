bc2479008737ab52412d3bef4af7a0efcd2d6aa4
Jie Wen <wenjie1070116@gmail.com>
2016-05-30 22:14:41 -0400

    [14. Longest Common Prefix][Accepted]committed by LeetCode Extension

--- 14-Longest-Common-Prefix/solution.py
+++ 14-Longest-Common-Prefix/solution.py
@@ -7,6 +7,8 @@ class Solution(object):
         if not strs or len(strs[0]) == 0: return '' # take care that strs[0] may be '' empty
         i = 0
         while True:
+            if i == len(strs[0]):
+                return strs[0][:i]
             target = strs[0][i]
             for j in xrange(1, len(strs)):
                 if i == len(strs[j]) or strs[j][i] != target:
