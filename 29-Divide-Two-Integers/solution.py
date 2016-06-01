546223764012bea5addc52410a96a0732825e6e0
Jie Wen <wenjie1070116@gmail.com>
2016-06-01 17:40:06 -0400

    [29. Divide Two Integers][Accepted]committed by LeetCode Extension

--- 29-Divide-Two-Integers/solution.py
+++ 29-Divide-Two-Integers/solution.py
@@ -10,17 +10,28 @@ class Solution(object):
         sign = 1
         if dividend < 0 and divisor > 0 or dividend > 0 and divisor < 0: sign = -1
         d, r = abs(dividend), abs(divisor)
-        if r == 1: return sign*d
-        res = 0
+        if d < r: return 0
+        temp = r
+        res = 1
+        while (temp << 1) <= d:
+            temp <<= 1
+            res <<= 1
+        d -= temp
+        temp >>= 1
+        idx = res
         while d >= r:
-            temp = r
-            idx = 1
-            while (temp << 1) <= r:
-                temp <<= 1
-                idx <<= 1
-            d = d-temp
-            res += idx
+            idx >>= 1
+            if d >= temp:
+                res |= idx
+                d -= temp
+            temp >>= 1
+        if sign*res > 2**31-1: return 2**31-1
+        if sign*res < -2**31: return -2**31
         return sign*res
+        
+            
+            
+        
                 
                 
         
