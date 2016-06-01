d87c0bdb81834d37af786e96d900f77ad76ee51d
Jie Wen <wenjie1070116@gmail.com>
2016-06-01 13:42:06 -0400

    [144. Binary Tree Preorder Traversal][Runtime Error]committed by LeetCode Extension

---
+++ 144-Binary-Tree-Preorder-Traversal/solution.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def preorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        if not root: return []
+        res = []
+        stack = [root]
+        while stack:
+            node = stack.pop()
+            res.append(node.val)
+            if node.right:
+                stack.append(node.right)
+            if node.left:
+                stack.apppend(node.left)
+        return res
+            
+        
