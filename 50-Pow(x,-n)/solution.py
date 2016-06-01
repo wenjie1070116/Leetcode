3ed07325ce2fb87812053fdb316f7ea38a9cc2cd
Jie Wen <wenjie1070116@gmail.com>
2016-06-01 14:53:35 -0400

    [50. Pow(x, n)][Accepted]committed by LeetCode Extension

--- 50-Pow(x,-n)/solution.py
+++ 50-Pow(x,-n)/solution.py
@@ -5,14 +5,9 @@ class Solution(object):
         :type n: int
         :rtype: float
         """
-        if x == 0:
-            return 0
         if n == 0:
             return 1
-        reverse = True if n < 0 else False
-        n = abs(n)
-        sign = -1 if x < 0 and abs(n)%2 == 1 else 1
-        res = self.myPow(x, n/2)**2*[x,1][n%2==0]
-        if reverse:
-            return 1/(sign*res)
-        return sign*res
+        if n == -1:
+            return 1/x
+        
+        return self.myPow(x*x, n/2)*[x,1][n%2==0]
