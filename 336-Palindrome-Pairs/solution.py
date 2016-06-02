8e6fc63553c03092bd9a378431757213e8a9af9c
Jie Wen <wenjie1070116@gmail.com>
2016-06-02 15:31:48 -0400

    [336. Palindrome Pairs][Accepted]committed by LeetCode Extension

--- 336-Palindrome-Pairs/solution.py
+++ 336-Palindrome-Pairs/solution.py
@@ -11,10 +11,12 @@ class Solution(object):
             hashmap[words[i]] = i
         for j in xrange(len(words)):
             word = words[j]
-            for k in xrange(len(word)+1):
+            # take care of the details, when '' exists or when another word == word[::-1]
+            for k in xrange(len(word)+1): # k's range
+                # remember to check whether remaining part is palindrome itself
                 if word[:k][::-1] in hashmap and hashmap[word[:k][::-1]] != j and word[k:]==word[k:][::-1]:
                     res.append([j, hashmap[word[:k][::-1]]])
-            for m in xrange(1,len(word)+1):
+            for m in xrange(1,len(word)+1): # m's range
                 if word[m:][::-1] in hashmap and hashmap[word[m:][::-1]] != j and word[:m]==word[:m][::-1]:
                     res.append([hashmap[word[m:][::-1]], j])
         return res
