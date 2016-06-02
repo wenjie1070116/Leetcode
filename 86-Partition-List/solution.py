3325efa7a55ac1a81f08440d0be14a3d576d8586
Jie Wen <wenjie1070116@gmail.com>
2016-06-02 00:58:30 -0400

    [86. Partition List][Runtime Error]committed by LeetCode Extension

---
+++ 86-Partition-List/solution.py
@@ -0,0 +1,29 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def partition(self, head, x):
+        """
+        :type head: ListNode
+        :type x: int
+        :rtype: ListNode
+        """
+        if not head or not head.next: return head
+        dummy = ListNode(0)
+        node = dummy
+        while node.next and node.next < x:
+                node = node.next
+        Next = node.next
+        while Next.next:
+            if Next.next.val >= x:
+                Next = Next.next
+            else:
+                temp = Next.next
+                Next.next = temp.next
+                temp.next = node.next
+                node.next = temp
+                node = node.next
+        return dummy.next
