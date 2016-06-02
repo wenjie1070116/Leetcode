1fdb555b8f5abf247fb1f87200f58fa079523bfb
Jie Wen <wenjie1070116@gmail.com>
2016-06-02 18:55:47 -0400

    [149. Max Points on a Line][Accepted]committed by LeetCode Extension

--- 149-Max-Points-on-a-Line/solution.py
+++ 149-Max-Points-on-a-Line/solution.py
@@ -3,7 +3,6 @@
 #     def __init__(self, a=0, b=0):
 #         self.x = a
 #         self.y = b
-import collections
 class Solution(object):
     def maxPoints(self, points):
         """
@@ -12,22 +11,22 @@ class Solution(object):
         """
         n = len(points) if points else 0
         if n < 3: return n
-        count = collections.Counter(points)
+        count = {}
+        for point in points:
+            count[(point.x, point.y)] = count.get((point.x, point.y), 0)+1
         unique_point = count.keys()
+        if len(unique_point) == 1: return count[unique_point[0]] #be careful when all the points is the same
         res = 0
-        for i in xrange(len(unique_point)):
+        for i in xrange(len(unique_point)-1):
             p1 = unique_point[i]
             hashmap = {}
             for j in xrange(i+1, len(unique_point)):
                 p2 = unique_point[j]
                 slop = None
-                if p2.x-p1.x == 0:
+                if p2[0]-p1[0] == 0:
                     slope = '#'
                 else:
-                    slope = (p2.y-p1.y)/(p2.x-p1.x)
-                hashmap[slope] = hashmap.get(slope, 0)+1
+                    slope = float(p2[1]-p1[1])/(p2[0]-p1[0])# take care the divide result should be float
+                hashmap[slope] = hashmap.get(slope, 0)+count[p2] #take care that p2 may have duplicate points
             res = max(res,max(hashmap.values())+count[p1])
         return res
-            
-        
-        
