f566365da723b4440952eca7188d7233cbcefbd1
Jie Wen <wenjie1070116@gmail.com>
2016-06-04 16:23:26 -0400

    [54. Spiral Matrix][Accepted]committed by LeetCode Extension

--- 54-Spiral-Matrix/solution.py
+++ 54-Spiral-Matrix/solution.py
@@ -8,34 +8,29 @@ class Solution(object):
         m, n = len(matrix), len(matrix[0])
         i, j = 0, 0
         res = []
-        count = 1
-        while count <= m*n:
-            while j < n and matrix[i][j] != 0:
+        while len(res) < m*n:
+            while j < n and matrix[i][j] != '#':
                 res.append(matrix[i][j])
-                matrix[i][j] = 0
+                matrix[i][j] = '#'
                 j += 1
-                count += 1
             j -= 1
             i += 1
-            while i < m and matrix[i][j] != 0:
+            while i < m and matrix[i][j] != '#':
                 res.append(matrix[i][j])
-                matrix[i][j] = 0
+                matrix[i][j] = '#'
                 i += 1
-                count += 1
             i -= 1
             j -= 1
-            while j >= 0 and matrix[i][j] != 0:
+            while j >= 0 and matrix[i][j] != '#':
                 res.append(matrix[i][j])
-                matrix[i][j] = 0
+                matrix[i][j] = '#'
                 j -= 1
-                count += 1
             j += 1
             i -= 1
-            while i >= 0 and matrix[i][j] != 0:
+            while i >= 0 and matrix[i][j] != '#':
                 res.append(matrix[i][j])
-                matrix[i][j] = 0
+                matrix[i][j] = '#'
                 i -= 1
-                count += 1
             i += 1
             j += 1
         return res
