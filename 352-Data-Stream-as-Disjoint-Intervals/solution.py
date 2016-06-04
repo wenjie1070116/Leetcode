c38e535edd85bc15e6f4012f81f04d86b187d1d0
Jie Wen <wenjie1070116@gmail.com>
2016-06-04 14:47:02 -0400

    [352. Data Stream as Disjoint Intervals][Accepted]committed by LeetCode Extension

--- 352-Data-Stream-as-Disjoint-Intervals/solution.py
+++ 352-Data-Stream-as-Disjoint-Intervals/solution.py
@@ -17,30 +17,36 @@ class SummaryRanges(object):
         :type val: int
         :rtype: void
         """
-        s = self.intervals
-        if not s or val > s[-1].end:
-            if val == s[-1].end + 1:
-                s[-1].end = val
+        if not self.intervals or val > self.intervals[-1].end:
+            if self.intervals and val == self.intervals[-1].end + 1:
+                self.intervals[-1].end = val
             else:
-                s.append(Interval(val, val)
-        elif val < s[0].start:
-            if val == s[0].start-1:
-                s[0].start = val
+                self.intervals.append(Interval(val, val))
+        elif val < self.intervals[0].start:
+            if val == self.intervals[0].start-1:
+                self.intervals[0].start = val
             else:
-                s = [Interval(val, val)]+s
+                self.intervals = [Interval(val, val)]+self.intervals
         else:
-            idx = bisect.bisect_left(Interval(val, val))
-            if s[idx-1].end == val or val == s[idx].start:
+            start, end = -1, len(self.intervals)
+            while start+1 < end:
+                mid = start+(end-start)/2
+                if self.intervals[mid].start < val:
+                    start = mid
+                else:
+                    end = mid
+            idx = end
+            if self.intervals[idx-1].end >= val or val == self.intervals[idx].start:
                 return
-            elif s[idx-1].end+1 == val == s[idx].start-1:
-                s[idx-1].end = s[idx].end
-                s.remove(s[idx])
-            elif s[idx-1].end+1 == val:
-                s[idx-1].end == val
-            elif s[idx].start-1 == val:
-                s[idx].start = val
+            elif val-1 == self.intervals[idx-1].end and val+1 == self.intervals[idx].start:
+                self.intervals[idx-1].end = self.intervals[idx].end
+                self.intervals.remove(self.intervals[idx])
+            elif val-1 == self.intervals[idx-1].end:
+                self.intervals[idx-1].end = val
+            elif val+1 == self.intervals[idx].start:
+                self.intervals[idx].start = val
             else:
-                s = s[:idx]+[Interval(val, val)]+s[idx:]
+                self.intervals = self.intervals[:idx]+[Interval(val, val)]+self.intervals[idx:]
 
     def getIntervals(self):
         """
