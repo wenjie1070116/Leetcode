05bc0bd079c3b0a79ec903786e9279115cfe6731
Jie Wen <wenjie1070116@gmail.com>
2016-06-05 19:51:27 -0400

    [41. First Missing Positive][Accepted]committed by LeetCode Extension

--- 41-First-Missing-Positive/solution.py
+++ 41-First-Missing-Positive/solution.py
@@ -8,7 +8,7 @@ class Solution(object):
             return 1
         for i in xrange(len(nums)):
             while nums[i] > 0 and nums[i]-1 < len(nums) and nums[i] != i+1 and nums[nums[i]-1] != nums[i]:
-                nums[i], nums[nums[i]-1] = nums[nums[i]-1], nums[i]
+                nums[nums[i]-1], nums[i] = nums[i], nums[nums[i]-1]
         for i in xrange(len(nums)):
             if nums[i] != i+1:
                 return i+1
