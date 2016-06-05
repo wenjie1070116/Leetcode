e0d8efbb2aac064a8fd5f45ad8fbc18b6fa64c65
Jie Wen <wenjie1070116@gmail.com>
2016-06-05 19:42:30 -0400

    [41. First Missing Positive][Time Limit Exceeded]committed by LeetCode Extension

--- 41-First-Missing-Positive/solution.py
+++ 41-First-Missing-Positive/solution.py
@@ -8,7 +8,9 @@ class Solution(object):
             return 1
         for i in xrange(len(nums)):
             while nums[i] > 0 and nums[i] != i+1 and nums[i]-1 < len(nums) and nums[i] != nums[nums[i]-1]:
-                nums[i], nums[nums[i]-1] = nums[nums[i]-1], nums[i]
+                temp = nums[i]
+                nums[i] = nums[nums[i]-1] 
+                nums[nums[i]-1] = temp
         for i in xrange(len(nums)):
             if nums[i] != i+1:
                 return i+1
