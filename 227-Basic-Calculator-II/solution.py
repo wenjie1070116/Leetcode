b40cf96c41033c2ad68a351f50f2877f85a1cd72
Jie Wen <wenjie1070116@gmail.com>
2016-06-04 22:10:06 -0400

    [227. Basic Calculator II][Accepted]committed by LeetCode Extension

--- 227-Basic-Calculator-II/solution.py
+++ 227-Basic-Calculator-II/solution.py
@@ -6,7 +6,7 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        s.strip(' ')
+        s = s.strip(' ')
         if not s: return 0
         def div(a, b):
             sign = 1 if a*b > 0 else -1
@@ -15,14 +15,14 @@ class Solution(object):
         S = re.split('(\D)', s)
         s = []
         for ele in S:
-            if ele != '' or ele != ' ':
+            if ele != '' and ele != ' ':
                 s.append(ele)
         nums = []
         ops = []
         idx = 0
         while idx < len(s):
             ch = s[idx]
-            if ch.isalnum() and ch != ' ':
+            if ch.isalnum():
                 nums.append(int(ch))
             elif ch in '+-*/':
                 if ch in '*/':
@@ -35,10 +35,7 @@ class Solution(object):
                 else:
                     ops.append(ch)
             idx += 1
-        while ops:
-            op = ops.pop()
-            b = nums.pop()
-            a = nums.pop()
-            nums.append(hashmap[op](a,b))
+        for i in xrange(len(ops)):
+            nums[0] = hashmap[ops[i]](nums[0], nums[i+1])
         return nums[0]
                     
