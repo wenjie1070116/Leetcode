a22acdbcd79a0933c6a634e25f0b409bd840cafb
Jie Wen <wenjie1070116@gmail.com>
2016-06-04 22:02:53 -0400

    [227. Basic Calculator II][Runtime Error]committed by LeetCode Extension

--- 227-Basic-Calculator-II/solution.py
+++ 227-Basic-Calculator-II/solution.py
@@ -1,15 +1,22 @@
 import operator
+import re
 class Solution(object):
     def calculate(self, s):
         """
         :type s: str
         :rtype: int
         """
+        s.strip(' ')
         if not s: return 0
         def div(a, b):
             sign = 1 if a*b > 0 else -1
             return sign*(abs(a)/abs(b))
         hashmap = {'+':operator.add, '-':operator.sub, '*':operator.mul, '/':div}
+        S = re.split('(\D)', s)
+        s = []
+        for ele in S:
+            if ele != '' or ele != ' ':
+                s.append(ele)
         nums = []
         ops = []
         idx = 0
@@ -20,6 +27,7 @@ class Solution(object):
             elif ch in '+-*/':
                 if ch in '*/':
                     a = nums.pop()
+                    idx += 1
                     while s[idx] == ' ':
                         idx += 1
                     b = int(s[idx])
