f2e1094a90a7ae5b2b07db41a0a4bff410d6c1e8
Jie Wen <wenjie1070116@gmail.com>
2016-06-05 17:48:13 -0400

    [40. Combination Sum II][Accepted]committed by LeetCode Extension

--- 40-Combination-Sum-II/solution.py
+++ 40-Combination-Sum-II/solution.py
@@ -1,3 +1,4 @@
+import bisect
 class Solution(object):
     def combinationSum2(self, candidates, target):
         """
@@ -7,13 +8,16 @@ class Solution(object):
         """
         if not candidates: return []
         candidates.sort()
+        self.idx = bisect.bisect_right(candidates, target)
         def dfs(pos, temp, res):
             if sum(temp) == target:
                 res.append([]+temp)
                 return
             if sum(temp) > target:
                 return
-            for i in xrange(pos, len(candidates)):
+            for i in xrange(pos, self.idx):
+                if i != pos and candidates[i] == candidates[i-1]:
+                    continue
                 temp.append(candidates[i])
                 dfs(i+1, temp, res)
                 temp.pop()
