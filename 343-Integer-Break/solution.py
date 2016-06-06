f6fcc3479ee255d6d6cd06f22aaaad66d4f90c4b
Jie Wen <wenjie1070116@gmail.com>
2016-06-06 16:44:49 -0400

    [343. Integer Break][Accepted]committed by LeetCode Extension

---
+++ 343-Integer-Break/solution.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def integerBreak(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n <= 0: return 0
+        dp = [0]*(n+1)
+        dp[1] = 1
+        for i in xrange(2, n+1):
+            for j in xrange(1, i/2+1):
+                dp[i] = max(dp[i], j*dp[i-j], dp[j]*(i-j), j*(i-j), dp[j]*dp[i-j])
+        return dp[n]
