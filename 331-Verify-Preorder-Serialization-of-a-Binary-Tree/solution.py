1d5fff6d772ab1b8c4c96cf49328e6c980896aa2
Jie Wen <wenjie1070116@gmail.com>
2016-06-06 17:01:26 -0400

    details, easy but also easy to make mistakes in code

--- 331-Verify-Preorder-Serialization-of-a-Binary-Tree/solution.py
+++ 331-Verify-Preorder-Serialization-of-a-Binary-Tree/solution.py
@@ -11,7 +11,7 @@ class Solution(object):
             if not stack or preorder[i] != '#' or stack[-1] != '#':
                 stack.append(preorder[i])
             else:
-                while stack and stack[-1] == '#':
+                while stack and stack[-1] == '#': # the while is very important since there maybe more than 1 '#' in the stack
                     stack.pop()
                     if not stack: return False
                     stack.pop()
