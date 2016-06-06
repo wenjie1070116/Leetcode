ace162eccbb59140ae713ef3dda479b92399b3c8
Jie Wen <wenjie1070116@gmail.com>
2016-06-06 16:45:17 -0400

    [343. Integer Break][Accepted]committed by LeetCode Extension

--- 343-Integer-Break/solution.py
+++ 343-Integer-Break/solution.py
@@ -9,5 +9,5 @@ class Solution(object):
         dp[1] = 1
         for i in xrange(2, n+1):
             for j in xrange(1, i/2+1):
-                dp[i] = max(dp[i], j*dp[i-j], dp[j]*(i-j), j*(i-j), dp[j]*dp[i-j])
+                dp[i] = max(dp[i], j*dp[i-j], dp[j]*(i-j), j*(i-j), dp[j]*dp[i-j]) # not just dp[j]*dp[i-j]
         return dp[n]
