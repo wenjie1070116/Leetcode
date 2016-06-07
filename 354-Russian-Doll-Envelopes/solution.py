03ab317e4b38c2621524d5556adfa3f0f30a9041
Jie Wen <wenjie1070116@gmail.com>
2016-06-07 13:49:49 -0400

    [354. Russian Doll Envelopes][Runtime Error]committed by LeetCode Extension

--- 354-Russian-Doll-Envelopes/solution.py
+++ 354-Russian-Doll-Envelopes/solution.py
@@ -5,10 +5,18 @@ class Solution(object):
         :rtype: int
         """
         if not envelopes: return 0
-        envelopes = sorted(envelopes, key = lambda x:x[0])
-        dp = [0]+[1]*len(envelopes)
+        envelopes = sorted(envelopes, cmp = lambda x,y:x[0]-y[0] if x[0]!=y[0] else y[1]-x[1])
+        dp = []
         for i in xrange(len(envelopes)):
-            for j in xrange(i):
-                if envelopes[i][0] > envelopes[j][0] and envelopes[i][1] > envelopes[j][1]:
-                    dp[i+1] = max(dp[i+1], dp[j+1]+1)
-        return max(dp)
+            if not dp or envelopes[i][1] > dp[-1][1]:
+                dp.append(envelopes[i])
+            else:
+                start, end = -1, len(dp)
+                while start + 1 < end:
+                    mid = start + (end-start)/2
+                    if dp[mid][1] > envelopes[i][1]:
+                        end = mid
+                    else:
+                        start = mid
+                dp[end] = envelopes[i]
+        return len(dp)
