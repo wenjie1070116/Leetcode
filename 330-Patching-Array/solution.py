6418a20aeb3ff440b72f21123d7281dac3cf4335
Jie Wen <wenjie1070116@gmail.com>
2016-06-06 22:46:32 -0400

    [330. Patching Array][Accepted]committed by LeetCode Extension

---
+++ 330-Patching-Array/solution.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def minPatches(self, nums, n):
+        """
+        :type nums: List[int]
+        :type n: int
+        :rtype: int
+        """
+        cur, total, res, i = 1, 0, 0, 0
+        while i < len(nums) and total < n:
+            if nums[i] > cur:
+                while total < n and nums[i] > cur:
+                    total += cur
+                    cur = total+1
+                    res += 1
+            else:
+                total += nums[i]
+                cur = total+1
+                i += 1
+        while total < n:
+            total += cur
+            cur = total+1
+            res += 1
+        return res
+        
