028dd580b1f87b01c16ea5ea3b742b5cc56a84db
Jie Wen <wenjie1070116@gmail.com>
2016-06-08 13:36:22 -0400

    [64. Minimum Path Sum][Accepted]committed by LeetCode Extension

--- 64-Minimum-Path-Sum/solution.py
+++ 64-Minimum-Path-Sum/solution.py
@@ -11,7 +11,6 @@ class Solution(object):
         for j in xrange(1, n):
             grid[0][j] += grid[0][j-1]
         for i in xrange(1, m):
-            for j in xrange(1, m):
+            for j in xrange(1, n):
                 grid[i][j] += min(grid[i-1][j], grid[i][j-1])
         return grid[-1][-1]
-        
