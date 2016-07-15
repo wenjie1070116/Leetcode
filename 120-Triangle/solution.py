895cc5b167eac895b0c521514c736ceba7ce3782
Jie Wen <wenjie1070116@gmail.com>
2016-07-15 13:18:09 -0700

    [120. Triangle][Runtime Error]committed by LeetCode Extension

---
+++ 120-Triangle/solution.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def minimumTotal(self, triangle):
+        """
+        :type triangle: List[List[int]]
+        :rtype: int
+        """
+        if not triangle: return 0
+        m = len(triangle)
+        for i in xrange(m-2, -1, -1):
+            for j in xrange(len(triangle[i+1])):
+                triangle[i][j] += min(triangle[i+1][j], triangle[i+1][j+1])
+        return triangle[0][0]
+        
