0a2f66ab4cc1e847ed8e5593bea7d2b1f7cf31b5
Jie Wen <wenjie1070116@gmail.com>
2016-07-15 12:41:24 -0700

    [130. Surrounded Regions][Runtime Error]committed by LeetCode Extension

--- 130-Surrounded-Regions/solution.py
+++ 130-Surrounded-Regions/solution.py
@@ -15,15 +15,17 @@ class Solution(object):
                 if 0<=nx<m and 0<=ny<n and board[nx][ny] == 'O':
                     change(nx, ny)
         
-        for i in (0, m-1):
-            for j in xrange(n):
-                if board[i][j] == 'O':
-                    change(i, j)
-        
-        for j in (0, n-1):
-            for i in xrange(1, m-1):
-                if board[i][j] == 'O':
-                    change(i, j)
+        for i in range(m):
+            if board[i][0] == 'O':
+                change(i, 0)
+            if board[i][n-1] == 'O':
+                change(i, n-1)
+            
+        for j in range(n):
+            if board[0][j] == 'O':
+                change(0, j)
+            if board[m-1][j] == 'O':
+                change(m-1, j)
         
         for i in xrange(m):
             for j in xrange(n):
