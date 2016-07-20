9527f745f161790f53049bb8a1f228ad21e5939c
Jie Wen <wenjie1070116@gmail.com>
2016-07-20 13:25:15 -0700

    [199. Binary Tree Right Side View][Time Limit Exceeded]committed by LeetCode Extension

---
+++ 199-Binary-Tree-Right-Side-View/solution.py
@@ -0,0 +1,29 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def rightSideView(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        if not root: return []
+        nodes = [root]
+        res = [root.val]
+        while nodes:
+            temp = []
+            for node in nodes:
+                if node.left:
+                    temp.append(node.left)
+                if node.right:
+                    temp.append(node.right)
+            if temp:
+                res.append(temp[-1].val)
+                nodes = temp
+        return res
+            
+        
