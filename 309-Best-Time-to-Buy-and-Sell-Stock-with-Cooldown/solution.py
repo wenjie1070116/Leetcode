31ebb1aa5bbec9e4e9c6a2ac8b285acd40f38a8a
Jie Wen <wenjie1070116@gmail.com>
2016-07-20 11:44:59 -0700

    [309. Best Time to Buy and Sell Stock with Cooldown][Runtime Error]committed by LeetCode Extension

---
+++ 309-Best-Time-to-Buy-and-Sell-Stock-with-Cooldown/solution.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        if not prices or len(prices) < 2: return 0
+        buy = [0, -prices[0]]
+        sell = [0, 0]
+        for i in xrange(2, len(prices)+1):
+            buy[i] = max(sell[i-2]-prices[i-1], buy[i-1]+(prices[i-2]-prices[i-1]))
+            sell[i] = max(buy[i-1]+prices[i-1], sell[i-1]+(prices[i-1]-prices[i-2]))
+        return max(buy[-1], sell[-1])
+        
