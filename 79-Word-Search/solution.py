18e22e900537ad353836f2f0032660ed1fc54003
Jie Wen <wenjie1070116@gmail.com>
2016-07-20 21:40:55 -0700

    [79. Word Search][Accepted]committed by LeetCode Extension

--- 79-Word-Search/solution.py
+++ 79-Word-Search/solution.py
@@ -8,18 +8,22 @@ class Solution(object):
         if not word: return True
         if not board: return False
         
-        def dfs(x,y,pos):
+        visited = set()
+        
+        def dfs(x,y,pos,visited):
             if pos == len(word)-1:
                 return True
+            visited.add((x,y))
             for dx, dy in zip((1, 0, -1, 0), (0, 1, 0, -1)):
                 nx, ny = x+dx, y+dy
-                if 0<=nx<len(board) and 0<=ny<len(board[0]) and board[nx][ny] = word[pos+1] and dfs(nx, ny, pos+1):
+                if 0<=nx<len(board) and 0<=ny<len(board[0]) and (nx, ny) not in visited and board[nx][ny] == word[pos+1] and dfs(nx, ny, pos+1, visited):
                     return True
+            visited.remove((x,y))
             return False
         
         for i in xrange(len(board)):
             for j in xrange(len(board[0])):
-                if board[i][j] == word[0] and dfs(i, j, 0):
+                if board[i][j] == word[0] and dfs(i, j, 0, visited):
                     return True
         return False
         
