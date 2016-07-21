20e6ba671a60c3c8011cf595e940db2ccf9de881
Jie Wen <wenjie1070116@gmail.com>
2016-07-20 21:02:08 -0700

    [209. Minimum Size Subarray Sum][Accepted]committed by LeetCode Extension

--- 209-Minimum-Size-Subarray-Sum/solution.py
+++ 209-Minimum-Size-Subarray-Sum/solution.py
@@ -10,13 +10,17 @@ class Solution(object):
         while right < len(nums):
             Sum += nums[right]
             if Sum >= s:
-                while Sum-nums[left] >= s:
+                while left < right and Sum-nums[left] >= s:
                     Sum -= nums[left]
                     left += 1
                 Min = min(Min, right-left+1)
+                if Min == 1 or right == len(nums)-1: return Min
+                Sum -= nums[left]
+                left += 1
+                right += 1
             else:
                 right += 1
-        return Min
+        return 0 if Min == sys.maxint else Min
                 
             
         
