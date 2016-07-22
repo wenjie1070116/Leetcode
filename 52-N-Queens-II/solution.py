c56e7c7eab561a6b1a318d6a2fd15e269a994524
Jie Wen <wenjie1070116@gmail.com>
2016-07-21 21:46:01 -0700

    [52. N-Queens II][Runtime Error]committed by LeetCode Extension

---
+++ 52-N-Queens-II/solution.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def totalNQueens(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n <= 0: return 0
+        left = [False]*(2*n)
+        right = [False]*(2*n)
+        vertical = [False]*n
+        def dfs(x, left, right, vertical, res):
+            if x == n:
+                res[0] += 1
+                return
+            for y in xrange(n):
+                if not left[x-y] and not right[x+y] and not vertical[y]:
+                    left[x-y] = True
+                    right[x+y] = True
+                    vertical[y] = True
+                    dfs(x+1, left, right, vertical)
+                    left[x-y] = False
+                    right[x+y] = False
+                    vertical[y] = False
+        res = [0]
+        dfs(0, left, right, vertical, res)
+        return res[0]
