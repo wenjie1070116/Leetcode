79f542d644a31fcb8f2b2cb4f4452b23aa10ed9c
Jie Wen <wenjie1070116@gmail.com>
2016-07-25 16:12:03 -0700

    [375. Guess Number Higher or Lower II][Accepted]committed by LeetCode Extension

---
+++ 375-Guess-Number-Higher-or-Lower-II/solution.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def getMoneyAmount(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        dp = [[0]*(n+1) for _ in xrange(n+1)]
+        for gap in xrange(1, n):
+            for lo in xrange(1, n+1-gap):
+                hi = lo+gap
+                dp[lo][hi] = min(x+max(dp[lo][x-1], dp[x+1][hi]) for x in range(lo, hi))
+        return dp[1][n]
+                
+            
