9e601d1549609cd3c06c346c499b9d6d55c00982
Jie Wen <wenjie1070116@gmail.com>
2016-07-26 12:21:33 -0700

    [365. Water and Jug Problem][Accepted]committed by LeetCode Extension

--- 365-Water-and-Jug-Problem/solution.py
+++ 365-Water-and-Jug-Problem/solution.py
@@ -1,9 +1,10 @@
 class Solution(object):
     def GCD(self, a, b):
-        Max = max(a, b)
-        Min = min(a, b)
-        if Max%Min == 0: return Min
-        return self.GCD(Max-Min, Min)
+        while a%b != 0:
+            temp = a-b
+            a = max(temp, b)
+            b = min(temp, b)
+        return b
     def canMeasureWater(self, x, y, z):
         """
         :type x: int
@@ -11,14 +12,15 @@ class Solution(object):
         :type z: int
         :rtype: bool
         """
+        if z == 0: return True
         if x == 0: return y==z
         if y == 0: return x==z
-        gcd = self.GCD(x, y)
-        X = x/gcd
-        Y = y/gcd
-        range_min = min(X, Y, [Y-X,X-Y][X>Y])*gcd
-        range_max = (X+Y)*gcd
-        if range_min<=z<=range_max:
+        gcd = self.GCD(x, y) if x > y else self.GCD(y, x)
+        if z%gcd != 0: return False
+        z /= gcd
+        x /= gcd
+        y /= gcd
+        if 0<=z<=x+y:
             return True
         return False
         
