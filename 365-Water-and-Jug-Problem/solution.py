39e188432b7605040bc26853120aab17631fca4a
Jie Wen <wenjie1070116@gmail.com>
2016-07-26 12:07:21 -0700

    [365. Water and Jug Problem][Runtime Error]committed by LeetCode Extension

---
+++ 365-Water-and-Jug-Problem/solution.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def GCD(self, a, b):
+        Max = max(a, b)
+        Min = min(a, b)
+        if Max%Min == 0: return Min
+        return self.GCD(Max-Min, Min)
+    def canMeasureWater(self, x, y, z):
+        """
+        :type x: int
+        :type y: int
+        :type z: int
+        :rtype: bool
+        """
+        gcd = self.GCD(x, y)
+        X = X/gcd
+        Y = Y/gcd
+        range_min = min(X, Y, [Y-X,X-Y][X>Y])*gcd
+        range_max = (X+Y)*gcd
+        if range_min<=z<=range_max:
+            return True
+        return False
+        
+        
