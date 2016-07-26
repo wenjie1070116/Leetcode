0aef0b3ded9fa02f4c02d221c465923dd5e6f877
Jie Wen <wenjie1070116@gmail.com>
2016-07-26 14:42:32 -0700

    [44. Wildcard Matching][Accepted]committed by LeetCode Extension

--- 44-Wildcard-Matching/solution.py
+++ 44-Wildcard-Matching/solution.py
@@ -5,33 +5,24 @@ class Solution(object):
         :type p: str
         :rtype: bool
         """
-        n = len(s)
-        m = len(p)
-        f = [[False]*(m+1) for i in range(n+1)]
-        f[0][0] = True
-
-        for i in range(1, n+1):
-            for j in range(1, m+1):
-                f[i][j] = (
-                    (f[i-1][j-1] and
-                    (s[i-1] == p[j-1] or p[j-1] == '?' or p[j-1] == '*')) or
-                    f[i-1][j] and p[j-1] == '*')
-
-        return f[n][m]
-        '''
-        if s == p:
-            return True
-        if not p:
-            return False
-        dp = [[False]*(len(p)+1) for _ in xrange(len(s)+1)]
-        dp[0][0] = True
-        if p[0] == '*':
-            dp[0][1] = True
-        for i in xrange(len(s)):
-            for j in xrange(len(p)):
-                if s[i] == p[j] or p[j] == '?' or p[j] == '*':
-                    dp[i+1][j+1] = dp[i][j]
-                elif p[j] == '*':
-                    dp[i+1][j+1] = dp[i][j+1]
-        return dp[-1][-1]
-        '''
+        if not s and not p: return True
+        if not p: return False
+        s_i, p_i = 0, 0
+        mark_p, mark_s = -1, -1
+        while s_i < len(s):
+            if p_i < len(p) and (s[s_i] == p[p_i] or p[p_i] == '?'):
+                s_i += 1
+                p_i += 1
+            elif p_i < len(p) and p[p_i] == '*':
+                mark_p = p_i
+                mark_s = s_i
+                p_i += 1
+            elif mark_p != -1:
+                p_i = mark_p + 1
+                mark_s += 1
+                s_i = mark_s
+            else:
+                return False
+        while p_i < len(p) and p[p_i] == '*':
+            p_i += 1
+        return p_i == len(p) and s_i == len(s)
