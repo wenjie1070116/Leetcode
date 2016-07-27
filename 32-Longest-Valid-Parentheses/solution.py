4df7a57b24e69ee8546cf88940b75e5df93f01c3
Jie Wen <wenjie1070116@gmail.com>
2016-07-27 15:47:56 -0700

    [32. Longest Valid Parentheses][Accepted]committed by LeetCode Extension

--- 32-Longest-Valid-Parentheses/solution.py
+++ 32-Longest-Valid-Parentheses/solution.py
@@ -8,11 +8,13 @@ class Solution(object):
         stack = []
         res = 0
         for i in xrange(len(s)):
-            if stack and s[i] == ')' and s[stack[-1]] == '(':
+            if stack and (s[i] == ')' and s[stack[-1]] == '('):
                 stack.pop()
             else:
                 stack.append(i)
-       stack.append(len(s))
-       for i in xrange(1, len(stack)):
-           res = max(res, stack[i]-stack[i-1]-1)
-       return res
+        if not stack: return len(s)
+        res = stack[0]
+        stack.append(len(s))
+        for i in xrange(1, len(stack)):
+            res = max(res, stack[i]-stack[i-1]-1)
+        return res
