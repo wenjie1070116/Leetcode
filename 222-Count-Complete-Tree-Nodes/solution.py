5c0400886a72cd6f5d7fc9b541a0a1f0031cf03b
Jie Wen <wenjie1070116@gmail.com>
2016-07-29 17:22:13 -0700

    Time Limit Exceeded

--- 222-Count-Complete-Tree-Nodes/solution.py
+++ 222-Count-Complete-Tree-Nodes/solution.py
@@ -6,15 +6,35 @@
 #         self.right = None
 
 class Solution(object):
-    def maxdepth(self, root):
-        if not root: return 0
-        return max(self.maxdepth(root.left), self.maxdepth(root.right))+1
-        
     def countNodes(self, root):
         """
         :type root: TreeNode
         :rtype: int
         """
+        if not root: return 0
+        level = 0
+        nodes = [root]
+        res = 0
+        while True:
+            level += 1
+            new = []
+            for node in nodes:
+                if node.left:
+                    new.append(node.left)
+                if node.right:
+                    new.append(node.right)
+            if len(new) != 2**level:
+                return 2**level-1+len(new)
+            nodes = new
+                    
+        
+    """
+    # Time Limit Exceeded
+    def maxdepth(self, root):
+        if not root: return 0
+        return max(self.maxdepth(root.left), self.maxdepth(root.right))+1
+        
+    def countNodes(self, root):
         if not root: return 0
         res = 0
         if self.maxdepth(root.left) == self.maxdepth(root.right):
@@ -24,4 +44,5 @@ class Solution(object):
             res += 2**(self.maxdepth(root.right))
             res += self.countNodes(root.left)
         return res
+    """
             
