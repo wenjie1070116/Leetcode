c70a04bb25945daa5c242d0a6b1af146c5219d57
Jie Wen <wenjie1070116@gmail.com>
2016-07-31 17:58:15 -0700

    [264. Ugly Number II][Accepted]committed by LeetCode Extension

--- 264-Ugly-Number-II/solution.py
+++ 264-Ugly-Number-II/solution.py
@@ -7,22 +7,15 @@ class Solution(object):
         if n <= 0: return 
         res = [1]
         hashmap = {2:0, 3:0, 5:0}
-        for i in xrange(1, n):
-            cur = None
-            if 3*res[hashmap[3]] >= 2*[hashmap[2]]:
-                cur = 2
-                if 3*res[hashmap[3]] == 2*[hashmap[2]]:
-                    hashmap[3] += 1
-            else:
+        while len(res) != n:
+            cur = 2
+            if 3*res[hashmap[3]] < cur*res[hashmap[cur]]:
                 cur = 3
-            
             if 5*res[hashmap[5]] < cur*res[hashmap[cur]]:
                 cur = 5
-            else:
-                if cur*res[hashmap[cur]] == 5*[hashmap[5]]:
-                    hashmap[5] += 1
-            res.append(cur*res[hashmap[cur]])
-            hahsmap[cur] += 1
+            if cur*res[hashmap[cur]] != res[-1]:
+                res.append(cur*res[hashmap[cur]])
+            hashmap[cur] += 1
         return res[-1]
                 
                 
